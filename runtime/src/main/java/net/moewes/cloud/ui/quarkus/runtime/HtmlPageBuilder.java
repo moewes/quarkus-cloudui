18a40798198cfdaf4fd6c1edd0beda3098f7e486
maik <moewes@users.noreply.github.com>
2020-08-14 21:44:47 +0200

    Improve HtmlPageBuilder

--- runtime/src/main/java/net/moewes/cloud/ui/quarkus/runtime/HtmlPageBuilder.java
+++ runtime/src/main/java/net/moewes/cloud/ui/quarkus/runtime/HtmlPageBuilder.java
@@ -11,14 +11,15 @@ public class HtmlPageBuilder {
 
   public String getPage(String view) {
 
-    String result =
+    String result = "<!doctype html>" +
         "<html>" +
-            "<head>";
-
+        "<head>" +
+        "<meta charset=\"utf-8\">" +
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
     String script = scripts.stream().map(item ->
         "<script src=\"" + item + "\"></script>").collect(Collectors.joining());
 
-    result = result + script +
+    result = result + script + getBasicStyle() +
         "</head>" +
         "<body><open-dxp-portlet backend=\"/" + view
         + "\"></open-dxp-portlet><body>";
@@ -28,5 +29,12 @@ public class HtmlPageBuilder {
 
   public void setScripts(List<String> scripts) {
     this.scripts = scripts;
+  }
+
+  private String getBasicStyle() {
+
+    return "<style>" +
+        "body { margin: 0; padding: 5px } " +
+        "</style>";
   }
 }
