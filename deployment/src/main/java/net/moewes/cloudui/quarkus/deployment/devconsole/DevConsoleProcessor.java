3fbd034ab95c795d6aecba49a0f85400026a9c0e
Maik <moewes@users.noreply.github.com>
2023-12-07 17:46:30 +0100

    adjust to quarkus 3.6

--- deployment/src/main/java/net/moewes/cloudui/quarkus/deployment/devconsole/DevConsoleProcessor.java
+++ deployment/src/main/java/net/moewes/cloudui/quarkus/deployment/devconsole/DevConsoleProcessor.java
@@ -2,24 +2,15 @@ package net.moewes.cloudui.quarkus.deployment.devconsole;
 
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
-import io.quarkus.devconsole.spi.DevConsoleRuntimeTemplateInfoBuildItem;
 import io.quarkus.devui.spi.JsonRPCProvidersBuildItem;
 import io.quarkus.devui.spi.page.CardPageBuildItem;
 import io.quarkus.devui.spi.page.Page;
 import net.moewes.cloudui.quarkus.deployment.ViewBuildItem;
 import net.moewes.cloudui.quarkus.runtime.dev.ViewInfoRpcService;
-import net.moewes.cloudui.quarkus.runtime.dev.ViewInfoSupplier;
 
 import java.util.List;
 
 public class DevConsoleProcessor {
-
-    @BuildStep(onlyIf = IsDevelopment.class)
-    public DevConsoleRuntimeTemplateInfoBuildItem collectBeanInfo(CurateOutcomeBuildItem curateOutcomeBuildItem) {
-        return new DevConsoleRuntimeTemplateInfoBuildItem("views",
-                new ViewInfoSupplier(), this.getClass(), curateOutcomeBuildItem);
-    }
 
     @BuildStep(onlyIf = IsDevelopment.class)
     CardPageBuildItem create(List<ViewBuildItem> viewBuildItems) {
--- runtime/src/main/java/net/moewes/cloudui/quarkus/runtime/CloudUiRecorder.java
+++ runtime/src/main/java/net/moewes/cloudui/quarkus/runtime/CloudUiRecorder.java
@@ -1,22 +1,22 @@
 package net.moewes.cloudui.quarkus.runtime;
 
-import java.util.List;
-import java.util.logging.Logger;
-
 import io.quarkus.arc.runtime.BeanContainer;
 import io.quarkus.runtime.annotations.Recorder;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
 import net.moewes.cloudui.quarkus.runtime.repository.View;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 @Recorder
 public class CloudUiRecorder {
 
     private static final Logger log = Logger.getLogger(CloudUiRecorder.class.getName());
-    
+
     public void touch(BeanContainer beanContainer, List<String> scripts) {
         log.info("register scripts ");
-        HtmlPageBuilder pageBuilder = beanContainer.instance(HtmlPageBuilder.class);
+        HtmlPageBuilder pageBuilder = beanContainer.beanInstance(HtmlPageBuilder.class);
         pageBuilder.setScripts(scripts);
     }
 
@@ -26,11 +26,12 @@ public class CloudUiRecorder {
     }
 
     public Handler<RoutingContext> getViewHandler(BeanContainer beanContainer) {
-        return new ViewRequestHandler(beanContainer, Thread.currentThread().getContextClassLoader());
+        return new ViewRequestHandler(beanContainer,
+                Thread.currentThread().getContextClassLoader());
     }
 
     public void registerView(BeanContainer beanContainer, View view) {
-        CloudUiRouter router = beanContainer.instance(CloudUiRouter.class);
+        CloudUiRouter router = beanContainer.beanInstance(CloudUiRouter.class);
         router.addView(view);
     }
 }
